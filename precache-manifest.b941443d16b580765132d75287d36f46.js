4eca692eeeb705003261102cd119c608d0a257d9
Anatoly Kobzisty <kobzistyy.a@gmail.com>
2020-01-05 23:31:17 +0600

    Updates

---
+++ precache-manifest.b941443d16b580765132d75287d36f46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f2f5e60fbdef441d945a5ca549d7be",
+    "url": "/countdown_timer/index.html"
+  },
+  {
+    "revision": "c4f454b5b33224406798",
+    "url": "/countdown_timer/static/css/2.2bcbc079.chunk.css"
+  },
+  {
+    "revision": "3318ff1d9347c1b5c79f",
+    "url": "/countdown_timer/static/css/main.cd05b13d.chunk.css"
+  },
+  {
+    "revision": "c4f454b5b33224406798",
+    "url": "/countdown_timer/static/js/2.a59e80ee.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/countdown_timer/static/js/2.a59e80ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3318ff1d9347c1b5c79f",
+    "url": "/countdown_timer/static/js/main.402d5139.chunk.js"
+  },
+  {
+    "revision": "5ac2e170f3a79d89dee6",
+    "url": "/countdown_timer/static/js/runtime-main.bf3863c6.js"
+  },
+  {
+    "revision": "a02296d12e8d370fa830ec3dca4d760b",
+    "url": "/countdown_timer/static/media/bell.a02296d1.mp3"
+  }
+]);
